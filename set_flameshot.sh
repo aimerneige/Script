f4860dd3768715f9f5aaf066ae9f3dbf32c9eba8
Aimer Neige <aimer.neige.soft@gmail.com>
2021-11-21 15:46:18 +0800

    update set_flameshot.sh (withoud test)

---
+++ set_flameshot.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0.binding 'Print'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0.command 'flameshot gui'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0.name 'flameshot'
